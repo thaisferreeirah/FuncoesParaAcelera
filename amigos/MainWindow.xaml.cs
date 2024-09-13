e5e3531d10fb034a2eae8aecf4f4844599055867
thais <thais.ferreira1795@gmail.com>
2024-09-13 13:53:42 -0300

    fotos ja estao dentro da ellipse

--- amigos/MainWindow.xaml.cs
+++ amigos/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -27,20 +28,23 @@ namespace Amigos
         public MainWindow()
         {
             InitializeComponent();
-            usuarioManager.AmazenarUsuario(0, "Thais", "imagens\\Thais.jpg");
-            usuarioManager.AmazenarUsuario(1, "Johnny", "imagens\\JK.png");
-            usuarioManager.AmazenarUsuario(2, "Lua", "imagens\\Lua.jpeg");
-            usuarioManager.AmazenarUsuario(3, "Pedro", "imagens\\Pedro.png");
+            usuarioManager.AmazenarUsuario(0, "Thais", "C:\\Users\\thais\\OneDrive\\Área de Trabalho\\acelera\\amigos\\Amigos\\imagens\\Thais.jpg");
+            usuarioManager.AmazenarUsuario(1, "Johnny", "C:\\Users\\thais\\OneDrive\\Área de Trabalho\\acelera\\amigos\\Amigos\\imagens\\JK.png");
+            usuarioManager.AmazenarUsuario(2, "Lua", "C:\\Users\\thais\\OneDrive\\Área de Trabalho\\acelera\\amigos\\Amigos\\imagens\\Lua.jpeg");
+            usuarioManager.AmazenarUsuario(3, "Pedro", "C:\\Users\\thais\\OneDrive\\Área de Trabalho\\acelera\\amigos\\Amigos\\imagens\\Pedro.png");
+            usuarioManager.AmazenarUsuario(4, "Puck", "C:\\Users\\thais\\OneDrive\\Área de Trabalho\\acelera\\amigos\\Amigos\\imagens\\Puck.png");
             
             codUsuario = 0;
             codAmigo = 1;
             posAmigo = 0;
              
             
+
             usuarioManager.AdicionarAmigo(codUsuario, 0);
             usuarioManager.AdicionarAmigo(codUsuario, 1);
             usuarioManager.AdicionarAmigo(codUsuario, 2);
             usuarioManager.AdicionarAmigo(codUsuario, 3);
+            usuarioManager.AdicionarAmigo(codUsuario, 4);
 
 
             for (int i = 0; i < usuarioManager.QuantAmigos(codUsuario); i++)
@@ -52,37 +56,37 @@ namespace Amigos
                 bitmap.EndInit();
                 if (posAmigo == 0)
                 {
-                    foto1.Source = bitmap;
+                    foto1.ImageSource = bitmap;
                     labelNome1.Content = usuarioManager.BuscarNome(codAmigo);
                     posAmigo++;
                 }
                 else if (posAmigo == 1)
                 {
-                    foto2.Source = bitmap;
+                    foto2.ImageSource = bitmap;
                     labelNome2.Content = usuarioManager.BuscarNome(codAmigo);
                     posAmigo++;
                 }
                 else if (posAmigo == 2)
                 {
-                    foto3.Source = bitmap;
+                    foto3.ImageSource = bitmap;
                     labelNome3.Content = usuarioManager.BuscarNome(codAmigo);
                     posAmigo++;
                 }
                 else if (posAmigo == 3)
                 {
-                    foto4.Source = bitmap;
+                    foto4.ImageSource = bitmap;
                     labelNome4.Content = usuarioManager.BuscarNome(codAmigo);
                     posAmigo++;
                 }
                 else if (posAmigo == 4)
                 {
-                    foto5.Source = bitmap;
+                    foto5.ImageSource = bitmap;
                     labelNome5.Content = usuarioManager.BuscarNome(codAmigo);
                     posAmigo++;
                 }
                 else if (posAmigo == 5)
                 {
-                    foto6.Source = bitmap;
+                    foto6.ImageSource = bitmap;
                     labelNome6.Content = usuarioManager.BuscarNome(codAmigo);
                     posAmigo++;
                 }
@@ -96,7 +100,7 @@ namespace Amigos
 
         private void botaoPerfil_Click(object sender, RoutedEventArgs e)
         {
-            Perfil perfil = new Perfil(codUsuario,3);
+            Perfil perfil = new Perfil(codUsuario,4);
             perfil.Show();
         }
     }
